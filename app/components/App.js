27fd3450287273fb00500031fcf3e5b082e14a39
Ricky <rick.levan@gmail.com>
2019-04-24 14:04:02 -0500

    make self-intersect game loss, also factor next block code out of setState

--- app/components/App.js
+++ app/components/App.js
@@ -48,6 +48,18 @@ outerLoop:
   }
 }
 
+function selfIntersect(snake) {
+  for (var i = 0; i < snake.length; i++) {
+    for (var j = i+1; j < snake.length; j++) {
+      if (snake[i][0] == snake[j][0] &&
+        snake[i][1] == snake[j][1]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 
 class App extends React.Component {
   constructor(props) {
@@ -60,6 +72,7 @@ class App extends React.Component {
       grid: getGreyGrid(numBlocks),
       snake: initSnake,
       pill: choosePill(initSnake),
+      fail: false,
       dir: "ArrowRight",
     };
 
@@ -83,37 +96,52 @@ class App extends React.Component {
   }
 
   tick() {
+    // do nothing if we've already lost
+    if (this.state.fail) {
+      return;
+    }
+    var oldx = this.state.snake[0][0];
+    var oldy = this.state.snake[0][1];
+
+    // alas, (old + 1) % numBlocks fails cuz JS '%' is messed up
+    if (this.state.dir == "ArrowDown") {
+      // more mirror-flip, sawry
+      oldy++;
+      if (oldy == numBlocks) {
+        oldy = 0;
+      }
+    } else if (this.state.dir == "ArrowUp") {
+      oldy--;
+      if (oldy == -1) {
+        oldy = numBlocks-1;
+      }
+    } else if (this.state.dir == "ArrowRight") {
+      oldx++;
+      if (oldx == numBlocks) {
+        oldx = 0;
+      }
+    } else {
+      oldx--;
+      if (oldx == -1) {
+        oldx = numBlocks-1;
+
+      }
+    }
+
+    var next = [oldx, oldy];
+    var cloneSnake = [...this.state.snake];
+
       this.setState( () => {
-        var oldx = this.state.snake[0][0];
-        var oldy = this.state.snake[0][1];
 
-        // alas, (old + 1) % numBlocks fails cuz JS '%' is messed up
-        if (this.state.dir == "ArrowDown") {
-          // more mirror-flip, sawry
-          oldy++;
-          if (oldy == numBlocks) {
-            oldy = 0;
-          }
-        } else if (this.state.dir == "ArrowUp") {
-          oldy--;
-          if (oldy == -1) {
-            oldy = numBlocks-1;
-          }
-        } else if (this.state.dir == "ArrowRight") {
-          oldx++;
-          if (oldx == numBlocks) {
-            oldx = 0;
-          }
-        } else {
-          oldx--;
-          if (oldx == -1) {
-            oldx = numBlocks-1;
 
+        if (selfIntersect([next].concat(cloneSnake))) {
+          return {
+            fail: true,
+            snake: [next].concat(cloneSnake),
           }
+
         }
 
-        var next = [oldx, oldy];
-        var cloneSnake = [...this.state.snake];
 
 
         // if head of snake intersects pill
@@ -153,7 +181,7 @@ class App extends React.Component {
                     {'height': '20px', 'width': '20px', background: (
                       /* mirror flip here, & j off by numBlocks */
                       getBlockColor(j-numBlocks, i, val, 
-                        this.state.snake, this.state.pill))}}>
+                        this.state.snake, this.state.pill, this.state.fail))}}>
                 </div>})}
               </div>);
             })}
@@ -164,9 +192,13 @@ class App extends React.Component {
 
 }
 
-function getBlockColor(i, j, gridVal, snake, pill) {
+function getBlockColor(i, j, gridVal, snake, pill, fail) {
   if (blockInSnake(i, j, snake)) {
-    return 'rgb(250,200,50)';
+    if (fail) {
+      return 'rgb(150,30,30)';
+    } else {
+      return 'rgb(250,200,50)';
+    }
   } else {
     if (pill[0] == i && pill[1] == j) {
       return 'rgb(255,255,255)';
